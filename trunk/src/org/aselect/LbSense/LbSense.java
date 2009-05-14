a316d9716c221e213669cf9374cb60df71e108be
jeroen <jeroen@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2009-05-14 14:17:35 +0000

    TrueMonitor renamed to LbSense

--- trunk/src/org/aselect/LbSense/LbSense.java
+++ trunk/src/org/aselect/LbSense/LbSense.java
@@ -5,34 +5,34 @@ import java.util.logging.Level;
 import org.aselect.system.error.Errors;
 import org.aselect.system.exception.ASelectException;
 
-public class TrueMonitor
+public class LbSense
 {
-	public final static String MODULE = "TrueMonitor";
+	public final static String MODULE = "LbSense";
 	
-	private static TrueMonitorSystemLogger _oTrueMonitorLogger;
+	private static LbSenseSystemLogger _oLbSenseLogger;
 	
-	TrueMonitorConfigManager _oConfigManager = null;
+	LbSenseConfigManager _oConfigManager = null;
 	
 	public static void main(String[] sArgs)
 	{
 		String sMethod = "main";
-		TrueMonitorSystemLogger oTrueMonitorSystemLogger = TrueMonitorSystemLogger.getHandle();
-		TrueMonitor oTrueMonitor = null;
+		LbSenseSystemLogger oLbSenseLogger = LbSenseSystemLogger.getHandle();
+		LbSense oLbSense = null;
 
 		try {
-			oTrueMonitor = new TrueMonitor();
-			oTrueMonitor.initialize();
-			oTrueMonitor.startServices();
+			oLbSense = new LbSense();
+			oLbSense.initialize();
+			oLbSense.startServices();
 
 			System.out.println("Successfully started" + MODULE);
-			oTrueMonitorSystemLogger.log(Level.SEVERE, MODULE, sMethod, "Successfully started" + MODULE);
+			oLbSenseLogger.log(Level.SEVERE, MODULE, sMethod, "Successfully started" + MODULE);
 		}
 		catch (Exception e) {
 			System.out.println("Failed to start" + MODULE + ", exception="+e);
-			oTrueMonitorSystemLogger.log(Level.SEVERE, MODULE, sMethod, "Failed to start " + MODULE, e);
+			oLbSenseLogger.log(Level.SEVERE, MODULE, sMethod, "Failed to start " + MODULE, e);
 
-			if (oTrueMonitor != null)
-				oTrueMonitor.destroy();
+			if (oLbSense != null)
+				oLbSense.destroy();
 
 			System.exit(1);
 		}
@@ -42,13 +42,13 @@ public class TrueMonitor
 	throws ASelectException
 	{
 		String sMethod = "initialize";
-		_oTrueMonitorLogger = TrueMonitorSystemLogger.getHandle();
+		_oLbSenseLogger = LbSenseSystemLogger.getHandle();
 		
 		// Get handle to the ConfigManager and initialize it
 		String sWorkingDir = System.getProperty("user.dir");
-		_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "Working dir=" + sWorkingDir);
+		_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "Working dir=" + sWorkingDir);
 		
-		_oConfigManager = TrueMonitorConfigManager.getHandle();
+		_oConfigManager = LbSenseConfigManager.getHandle();
 		_oConfigManager.init(sWorkingDir);
 		
 		// Get our main section
@@ -56,9 +56,9 @@ public class TrueMonitor
 
 		// Initialize the system logger
 		Object oLogSection = _oConfigManager.getSectionFromSection(oMainSection, "logging", "id=system", true);
-		_oTrueMonitorLogger.init(_oConfigManager, oLogSection, sWorkingDir);
+		_oLbSenseLogger.init(_oConfigManager, oLogSection, sWorkingDir);
 		// Logging goes to the system logfile now
-		_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "Starting True Monitor");
+		_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "Starting True Monitor");
 	}
 	
 	public void startServices()
@@ -66,14 +66,14 @@ public class TrueMonitor
 	{
 		String sMethod = "startServices";
 
-		_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "====");
+		_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "====");
 		Object oHandlers = _oConfigManager.getSimpleSection(null, "handlers", true);
 		Object oConfigHandler = _oConfigManager.getSimpleSection(oHandlers, "handler", true);
 
 		for ( ; oConfigHandler != null; ) {
 			String sId = _oConfigManager.getParam(oConfigHandler, "id");
 			String sClass = _oConfigManager.getParam(oConfigHandler, "class");
-			_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "id="+sId+" class="+sClass);
+			_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "id="+sId+" / class="+sClass);
             
 			Class cTrueHandler = null;
             ISenseHandler oSenseHandler = null;
@@ -82,22 +82,22 @@ public class TrueMonitor
                 oSenseHandler = (ISenseHandler)cTrueHandler.newInstance();
             }
             catch (Exception e) {
-            	_oTrueMonitorLogger.log(Level.WARNING, MODULE, sMethod, "Cannot instantiate class '" + sClass + "'", e);
+            	_oLbSenseLogger.log(Level.WARNING, MODULE, sMethod, "Cannot instantiate class '" + sClass + "'", e);
                 throw new ASelectException(Errors.ERROR_ASELECT_INIT_ERROR, e);
             }
             
             oSenseHandler.initialize(oConfigHandler);
     		Thread _tMyServiceHandler = new Thread(oSenseHandler);
-			_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "run handler");
+			_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "run handler");
     		_tMyServiceHandler.start();  // don't use run() here!
     		oConfigHandler = _oConfigManager.getNextSection(oConfigHandler);
-			_oTrueMonitorLogger.log(Level.INFO, MODULE, sMethod, "handler="+oConfigHandler);
+			_oLbSenseLogger.log(Level.INFO, MODULE, sMethod, "handler="+oConfigHandler);
 		}
 	}
 	
 	public void destroy()
 	{
 		String sMethod = "destroy";
-		_oTrueMonitorLogger.log(Level.SEVERE, MODULE, sMethod, "Stopping all components.");
+		_oLbSenseLogger.log(Level.SEVERE, MODULE, sMethod, "Stopping all components.");
 	}
 }
