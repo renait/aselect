99b5e83f04ded8fcfcf84e09f09a8cf8cca13c5a
remy <remy@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2011-01-04 16:31:30 +0000

    Implementation of wireless-services sms gateway Form signature in sms authentication form to prevent tampering with retry_count

---
+++ src/org/aselect/authspserver/authsp/sms/SmsSenderFactory.java
@@ -0,0 +1,63 @@
+/**
+ *  Copyright (c) Anoigo. All rights reserved.
+ *
+ * A-Select is a trademark registered by SURFnet bv.
+ *
+ * This program is distributed under the EUPL 1.0 (http://osor.eu/eupl)
+ * See the included LICENSE file for details.
+ *
+ * If you did not receive a copy of the LICENSE
+ * please contact Anoigo. (http://www.anoigo.nl) 
+ */
+package org.aselect.authspserver.authsp.sms;
+
+import java.net.URL;
+
+/**
+ * * A simple factory that creates SmsSender for various outbound sms providers <br>
+ * <br>
+ * <b>Description:</b>
+ * Factory that can create an SmsSender depending on supplied parameter (mollie, wireless-services)  
+ * defaults to "mollie" provider
+ * <br>
+ * <b>Concurrency issues:</b> <br>
+ * - <br>
+ * @author RH
+ *
+ */
+public class SmsSenderFactory
+{
+	private static final String PROVIDER_WIRELESSSERVICES = "wireless-services";
+
+	/**
+	 * * creator method <br>
+	 * <br>
+	 * <b>Description:</b>
+	 * Creates an SmsSender depending on supplied provider parameter (mollie, wirelessservices)  
+	 * defaults to "mollie" provider
+	 * <br>
+	 * <br>
+	 * 	 @param url
+	 * 		The sms gateway provider url. May be https if appropriate certificate is loaded in cacerts
+	 * 	 @param user
+	 * 		sms gateway account username
+	 * 	 @param password
+	 * 		sms gateway account password
+	 * 	 @param gateway
+	 * 		optional priority level, not supported by all sms providers
+	 * 	 @param gw_provider
+	 * 		identifier for sms gateway provider, currently [ wireless-services | mollie ] defaults to mollie
+	 * 
+	 * <b>Concurrency issues:</b> <br>
+	 * - <br>
+	 */
+	public static SmsSender createSmsSender(URL url, String user, String password, String gateway, String gw_provider) {
+		if (PROVIDER_WIRELESSSERVICES.equalsIgnoreCase(gw_provider)) {
+			return new  WirelessServicesHttpSmsSender(url, user, password, gateway);
+		}
+		else {	// default to "mollie"
+			return new  MollieHttpSmsSender(url, user, password, gateway);
+		}
+		
+	}
+}
---
+++ src/org/aselect/authspserver/authsp/sms/WirelessServicesHttpSmsSender.java
@@ -0,0 +1,176 @@
+/*
+ * * Copyright (c) Anoigo. All rights reserved.
+ *
+ * A-Select is a trademark registered by SURFnet bv.
+ *
+ * This program is distributed under the EUPL 1.0 (http://osor.eu/eupl)
+ * See the included LICENSE file for details.
+ *
+ * If you did not receive a copy of the LICENSE
+ * please contact Anoigo. (http://www.anoigo.nl) 
+ */
+package org.aselect.authspserver.authsp.sms;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+
+import org.aselect.authspserver.log.AuthSPSystemLogger;
+import org.aselect.system.utils.Tools;
+
+/**
+ * Sends request for sms to wireless-services sms gateway
+ * @author RH
+ * 
+ */
+public class WirelessServicesHttpSmsSender implements SmsSender
+{
+	private static final String SEPCHAR = "=";
+	private static final String GWVERSION = "1.1";
+	private final String user;
+	private final String password;
+	private final URL url;
+	private final String gateway;
+
+	/**
+	 * Instantiates a new wirelessservices http sms sender.
+	 * 
+	 * @param url
+	 *            the sms gateway provider url
+	 * @param user
+	 *            the account user
+	 * @param password
+	 *            the account password
+	 */
+	public WirelessServicesHttpSmsSender(URL url, String user, String password) {
+		this(url, user, password, null);
+	}
+
+	/**
+	 * Instantiates a new wirelessservices http sms sender.
+	 * 
+	 * @param url
+	 *            the sms gateway provider url
+	 * @param user
+	 *            the account user
+	 * @param password
+	 *            the account password
+	 * @param gateway
+	 *            the (optional) priority gateway if supported by the sms gateway provider
+	 */
+	public WirelessServicesHttpSmsSender(URL url, String user, String password, String gateway) {
+		super();
+		this.url = url;
+		this.user = user;
+		this.password = password;
+		this.gateway = gateway;
+	}
+
+	/**
+	 * @param message
+	 * 		the body of the message to send
+	 * @param from
+	 * 		the 'from' info to put in the message, can be alpha (max 11 chars) or numeric (max. 16 digits)
+	 * @param recipients
+	 * 		comma seperated list of recipient numbers
+	 * @return progress step start=15, 19=finished
+	 * 
+	 * (non-Javadoc)
+	 * @see org.aselect.authspserver.authsp.sms.SmsSender#sendSms(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	public int sendSms(String message, String from, String recipients)
+		throws SmsException
+	{
+		int returncode = 15;
+		StringBuffer data = new StringBuffer();
+		AuthSPSystemLogger _systemLogger;
+		_systemLogger = AuthSPSystemLogger.getHandle();
+
+		try {
+			final String EQUAL_SIGN = SEPCHAR;
+			final String AMPERSAND = "&";
+			data.append(URLEncoder.encode("VERSION", "UTF-8"));
+			data.append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(GWVERSION, "UTF-8"));
+			data.append(AMPERSAND);
+			data.append(URLEncoder.encode("UID", "UTF-8"));
+			data.append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(this.user, "UTF-8"));
+			data.append(AMPERSAND);
+			data.append(URLEncoder.encode("PWD", "UTF-8"));
+			data.append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(this.password, "UTF-8"));
+			data.append(AMPERSAND);
+			data.append(URLEncoder.encode("O", "UTF-8")).append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(from, "UTF-8"));
+			data.append(AMPERSAND);
+			data.append(URLEncoder.encode("M", "UTF-8")).append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(message, "UTF-8"));
+			data.append(AMPERSAND);
+			data.append(URLEncoder.encode("N", "UTF-8")).append(EQUAL_SIGN);
+			data.append(URLEncoder.encode(recipients, "UTF-8"));
+
+			// gateway == null, Wireless Services cannot specify alternate gateway like this, must use other URL
+			if (this.gateway != null && !"".equals(this.gateway.trim())) {
+				_systemLogger.log(Level.WARNING, "Wireless Services does not support alternate gateway, you must use alternate URL for this");
+			}
+			_systemLogger.log(Level.INFO, "Wireless Services", "sendSms", "url=" + url.toString() + " data=" + data.toString());
+			returncode++; // 16
+//			URLConnection conn = url.openConnection();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//			conn.setRequestProperty("Connection", "close"); // use this if we will explicitly conn.disconnect();
+			conn.setRequestMethod("POST");	// Wireless Services prefers POST
+			conn.setRequestProperty("Host", url.getHost());	// Wireless Services requires 'Host' header
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");	// Wireless Services requires this for POST
+			conn.setReadTimeout(10000);
+			returncode++; // 17
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.write(data.toString());
+			wr.flush();
+			returncode++; // 18
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			String sResult = "", sResultCode = "";
+			while ((line = rd.readLine()) != null) {	// there should be only one significant line, ignore extra lines
+				if ( "".equals(sResult) && !"".equals(line) ) sResult = line;	// get first non-empty line
+			}
+			_systemLogger.log(Level.INFO, "Wireless Services", "sendSms", "result:" + sResult);
+
+			int resLength = sResult.length();
+			if (resLength == 0) {
+				throw new SmsException("Wireless Services may not have send sms, no returncode from Wireless Services");
+			}
+			int sepPos = sResult.indexOf(SEPCHAR);
+			if (sepPos == -1) {
+				throw new SmsException("Wireless Services may not have send sms, no '" + SEPCHAR + "' seperator received from Wireless Services");
+			}
+			sResultCode = sResult.substring(0, sepPos);
+			if (!sResultCode.startsWith("0")) {
+				throw new SmsException("Wireless Services could not send sms, returncode from Wireless Services: " + sResultCode);
+			}
+			
+			returncode++; // 19
+			wr.close();
+			rd.close();
+		}
+		catch (NumberFormatException e) {
+			throw new SmsException("Sending SMS, using \'" + this.url.toString()
+					+ "\' failed due to number format exception! " + e.getMessage(), e);
+		}
+		catch (Exception e) {
+			throw new SmsException("Sending SMS, using \'" + this.url.toString() + "\' failed (progress=" + returncode
+					+ ")! " + e.getMessage(), e);
+		}
+		return returncode;
+	}
+	
+}
