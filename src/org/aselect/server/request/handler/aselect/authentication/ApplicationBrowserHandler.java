91519b8d08c404f22eba3f185eec35b87958df45
bauke <bauke@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2012-10-02 18:14:12 +0000

    logout_return_url fix. mechanism was broken.

--- src/org/aselect/server/request/handler/aselect/authentication/ApplicationBrowserHandler.java
+++ src/org/aselect/server/request/handler/aselect/authentication/ApplicationBrowserHandler.java
@@ -2192,7 +2192,7 @@ public class ApplicationBrowserHandler extends AbstractBrowserRequestHandler
 					((sRemoteAsUrl!=null)? URLDecoder.decode(sRemoteAsUrl, "UTF-8"): "null")+" _sMyOrg="+_sMyOrg);
 			
 			// 20120929, Bauke: only allow a redirect from our server when the user was logged in!!
-			if (sRemoteAsUrl != null && _htSessionContext != null) {
+			if (sRemoteAsUrl != null && _htTGTContext != null) {
 				Tools.pauseSensorData(_configManager, _systemLogger, _htSessionContext);  //20111102
 				// no RID: _sessionManager.update(sRid, _htSessionContext); // Write session
 				_sessionManager.setUpdateSession(_htSessionContext, _systemLogger);  // 20120401, Bauke: added, was update()
