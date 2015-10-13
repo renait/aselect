528a141677ae290e030b6923b6b0d1943240a5c7
vojenfr <vojenfr@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2015-10-13 13:58:33 +0000

    test

--- trunk/src/org/aselect/system/communication/client/raw/RawCommunicator.java
+++ trunk/src/org/aselect/system/communication/client/raw/RawCommunicator.java
@@ -92,6 +92,7 @@ package org.aselect.system.communication.client.raw;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.IOException2_test;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
