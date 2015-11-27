002e6765aab9aae6d23f5dd2c7d7734691649c89
remy <remy@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2015-11-27 10:29:29 +0000

    Added some notices

--- trunk/src/org/aselect/server/authspprotocol/handler/RDAAuthSPHandler.java
+++ trunk/src/org/aselect/server/authspprotocol/handler/RDAAuthSPHandler.java
@@ -1,4 +1,13 @@
-/* 
+/*
+ * * Copyright (c) Anoigo. All rights reserved.
+ *
+ * A-Select is a trademark registered by SURFnet bv.
+ *
+ * This program is distributed under the EUPL 1.0 (http://osor.eu/eupl)
+ * See the included LICENSE file for details.
+ *
+ * If you did not receive a copy of the LICENSE
+ * please contact Anoigo. (http://www.anoigo.nl) 
  *
  * 20150501 - Adapted fromSMSAuthSPHandler, implementing RDA (Remote Document Authentication)
  * @author RH - www.anoigo.nl
