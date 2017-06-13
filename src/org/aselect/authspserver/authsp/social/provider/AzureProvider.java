1640c5a7d714654a55f18f7bbec2bc40a2d601d0
remy <remy@e41161e8-a2e8-11e7-95c3-191e0f288cfb>
2017-06-13 12:32:05 +0000

    social authsp AzureProvider commit problem

--- src/org/aselect/authspserver/authsp/social/provider/AzureProvider.java
+++ src/org/aselect/authspserver/authsp/social/provider/AzureProvider.java
@@ -24,3 +24,31 @@
 20170216, RH
 Adjusted for OpenID Connect
  */
+
+
+package org.aselect.authspserver.authsp.social.provider;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aselect.authspserver.authsp.social.oauthstrategy.OpenIDConnect;
+import org.brickred.socialauth.AbstractProvider;
+import org.brickred.socialauth.AuthProvider;
+import org.brickred.socialauth.Contact;
+import org.brickred.socialauth.Permission;
+import org.brickred.socialauth.Profile;
+import org.brickred.socialauth.exception.AccessTokenExpireException;
+import org.brickred.socialauth.exception.SocialAuthException;
+import org.brickred.socialauth.exception.UserDeniedPermissionException;
+import org.brickred.socialauth.oauthstrategy.OAuthStrategyBase;
+import org.brickred.socialauth.util.AccessGrant;
+import org.brickred.socialauth.util.Constants;
+import org.brickred.socialauth.util.OAuthConfig;
+import org.brickred.socialauth.util.Response;
